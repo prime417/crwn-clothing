05a71dfa615a58ace4bb45914dda6735843c6baa
Pankaj Salunkhe <pankaj.salunkhe@everstek.com>
2020-11-22 20:23:20 +0530

    Add withRouter changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
+
+const JacketsPage = () => (
+  <div>
+    <h1>JACKETS PAGE </h1>
+  </div>
+);
+
+const MensPage = () => (
+  <div>
+    <h1>MENS PAGE</h1>
+  </div>
+);
+
+const WomensPage = () => (
+  <div>
+    <h1>WOMENS PAGE</h1>
+  </div>
+);
+
+const SneakersPage = () => (
+  <div>
+    <h1>SNEAKERS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+        <Route path='/jackets' component={JacketsPage} />
+        <Route path='/sneakers' component={SneakersPage} />
+        <Route path='/womens' component={WomensPage} />
+        <Route path='/mens' component={MensPage} />
+      </Switch>
     </div>
   );
 }
